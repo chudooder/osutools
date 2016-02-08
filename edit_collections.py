c97d370e565e762d042bdf004a504d8b4af05307
Shawn <codeshawnachu@gmail.com>
2016-02-08 12:46:24 -0600

    improved error handling

--- edit_collections.py
+++ edit_collections.py
@@ -27,40 +27,31 @@ class BeatmapFilter:
     def __init__(self, fld, op, val):
         self.field = fld
         self.operator = op
-
-        valtype = FILTER_MAP[self.field]['type']
-        if valtype == 'float':
-            self.value = float(val)
-        elif valtype == 'string':
-            self.value = val
+        self.value = val
 
     def check(self, bm):
-        try:
-            bmval = bm[FILTER_MAP[self.field]['key']]
-            valtype = FILTER_MAP[self.field]['type']
+        bmval = bm[FILTER_MAP[self.field]['key']]
+        valtype = FILTER_MAP[self.field]['type']
 
-            if valtype == 'float':
-                bmval = float(bmval)
+        if valtype == 'float':
+            bmval = float(bmval)
 
-            if self.operator == '=' or self.operator == '==':
-                return bmval == self.value
-            elif self.operator == '<':
-                return bmval < self.value
-            elif self.operator == '<=':
-                return bmval <= self.value
-            elif self.operator == '>':
-                return bmval > self.value
-            elif self.operator == '>=':
-                return bmval >= self.value
-            elif self.operator == '!=':
-                return bmval != self.value
-            elif self.operator == '?':
-                return self.value in bmval
-            elif self.operator == '!?':
-                return self.value not in bmval
-
-        except TypeError:
-            print "Invalid type: " + self.toString()
+        if self.operator == '=' or self.operator == '==':
+            return bmval == self.value
+        elif self.operator == '<':
+            return bmval < self.value
+        elif self.operator == '<=':
+            return bmval <= self.value
+        elif self.operator == '>':
+            return bmval > self.value
+        elif self.operator == '>=':
+            return bmval >= self.value
+        elif self.operator == '!=':
+            return bmval != self.value
+        elif self.operator == '?':
+            return self.value in bmval
+        elif self.operator == '!?':
+            return self.value not in bmval
 
     def toString(self):
         return self.field + ' ' + self.operator + ' ' + str(self.value)
@@ -78,44 +69,122 @@ def writeCollectionDB(collections):
 
     out.close()
     
-    
+
 
 def acceptInput(collections, bmJson):
     # print out list of current collections
     cmd = ''
     beatmaps = bmJson['beatmaps']
-    while cmd != 'done':
-        i = 0
-        for cname, hashes in collections['collections'].iteritems():
-            print str(i) + ' - ' + cname + ': '
-            i += 1
-            for md5 in hashes:
-                if md5 not in beatmaps:
-                    print 'Could not find local beatmap: ' + md5
-                bm = beatmaps[md5]
-                print bm['song_title'] + ' ['+bm['version']+']'
+    done = False
+    while not done:
+        cnames = collections['collections'].keys()
 
         print '------------------'
-        cmd = raw_input('>')
+        for i, cname in enumerate(cnames):
+            nummaps = len(collections['collections'][cname])
+            print '{} - {:25}: {} beatmaps'.format( \
+                str(i), cname, nummaps)
+        print '------------------'
+
+        cmd = raw_input('> ')
         
         if cmd == 'add':
-            cname = raw_input('Collection name: ')
-            filters = []
-            fstring = ""
-            while True:
-                fstring = raw_input('Filter: ')
-                if fstring == 'done':
-                    break
+            addCollection(cmd, collections, beatmaps)
 
-                sp = fstring.split(' ', 3)
-                filters.append(BeatmapFilter(sp[0], sp[1], sp[2]))
+        if cmd.startswith('remove'):
+            removeCollection(cmd, collections, beatmaps)
 
-            collection = [bm for bm in filterBeatmaps(beatmaps, filters)]
-            collections['collections'][cname] = collection
+        elif cmd.startswith('list'):
+            listCollection(cmd, collections, beatmaps)
 
-    writeCollectionDB(collections)
+        elif cmd.startswith('save'):
+            writeCollectionDB(collections)
+
+        elif cmd.startswith('quit'):
+            res = raw_input('Save? Y/N: ')
+            if res.upper() == 'Y':
+                writeCollectionDB(collections)
+            elif res.upper() == 'N':
+                done = True
+
             
-                
+def addCollection(cmd, collections, beatmaps):
+    print('Collection name:')
+    cname = raw_input('> ')
+    filters = []
+    fstring = ""
+    while True:
+        print('Filter:')
+        fstring = raw_input('> ')
+        if fstring == 'done':
+            break
+
+        sp = fstring.split(' ', 3)
+        if len(sp) < 3:
+            print 'Usage: <filter> [=, ==, !=, <, >, <=, >=, ?, !?] <val>'
+            print 'Or type \'done\' to exit'
+            continue
+
+        field = sp[0].upper()
+        if field not in FILTER_MAP:
+            print 'Error: not a valid filter'
+            continue
+
+        oper = sp[1]
+        if oper not in ['=', '==', '!=', '<', '>', '<=', '>=', '?', '!?']:
+            print 'Error: Invalid operator'
+            continue
+
+        valtype = FILTER_MAP[field]['type']
+        val = sp[2]
+        if valtype == 'float':
+            try:
+                val = float(sp[2])
+            except ValueError:
+                print 'Error: value should be a float'
+                continue
+
+
+        filters.append(BeatmapFilter(field, oper, val))
+
+    collection = [bm for bm in filterBeatmaps(beatmaps, filters)]
+    collections['collections'][cname] = collection
+
+def removeCollection(cmd, collections, beatmaps):
+    try:
+        index = int(cmd.split(' ')[1])
+    except ValueError:
+        print "Usage: remove <index>"
+        return
+
+    cnames = collections['collections'].keys()
+    if index not in range(len(cnames)):
+        print "Index out of range: " + str(index)
+        return
+
+    del collections['collections'][cnames[index]]
+
+def listCollection(cmd, collections, beatmaps):
+    try:
+        index = int(cmd.split(' ')[1])
+    except ValueError:
+        print "Usage: list <index>"
+        return
+
+    cnames = collections['collections'].keys()
+    if index not in range(len(cnames)):
+        print "Index out of range: " + str(index)
+        return
+
+    for md5 in collections['collections'][cnames[index]]:
+        if md5 not in beatmaps:
+            print 'Could not find local beatmap: ' + md5
+        else:
+            bm = beatmaps[md5]
+            print bm['song_title'] + ' ['+bm['version']+']'
+
+    print '* Press ENTER to continue...'
+    raw_input()
 
 # returns a collection of beatmaps that match the
 # given filter criteria
@@ -127,8 +196,8 @@ if __name__ == '__main__':
     # c = {'version': '20151212', 'collections': {}}
     # writeCollectionDB(c)
 
-    osuDb = open('data/osu!.db')
-    collectionsDb = open('data/collection.db')
+    osuDb = open('data/osu!.db', 'rb')
+    collectionsDb = open('data/collection.db', 'rb')
 
     collections = dbparse.parseCollectionsDb(collectionsDb.read())
     beatmaps = dbparse.parseOsuDb(osuDb.read())
